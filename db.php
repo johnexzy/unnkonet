1baa687e8f4d5abb2867fe00e4198c2bcffa773f
Johnexzy <obajohn75@gmail.com>
2020-06-06 22:36:18 +0100

    modified

--- db.php
+++ db.php
@@ -1,28 +1,9 @@
 <?php
-    try{
-        $conn = new PDO("mysql:host=localhost;dbname=mysqli_login", "root", "");
-        if ($_GET) {
-            $header = $_GET['header'];
-            $msg = $_GET['msg'];
-            $avatar_path = 'images/'.$_FILES['avatar']['name'];
-            if (preg_match("!image!", $_FILES['avatar']['type'])) {
-                if (copy($_FILES['avatar']['tmp_name'], $avatar_path)){
-            $insert = $conn->prepare("INSERT INTO showcase (header, msg, avatar_path) values(:header, :msg, :image)");
-            $insert->bindParam(':header', $header);
-            $insert->bindParam(':msg', $msg);
-            $insert->bindParam(':image', $avatar_path);
-            $insert->execute(); 
-            $out = 'succesful';
-            echo $out;
-                }
-            }
-        }else
-        {
-            echo "error";
-        }
-    }
-    catch(PDOException $e){
-        echo "error" .$e->getMessage();
-    }
-    
-?>
+$err = '127.0.0.1:8900/www/views/carousel';
+$uri = explode('/', $err);
+echo $uri[0]."\n";
+echo $uri[1]."\n";
+echo $uri[2]."\n";
+echo $uri[3]."\n";
+$nowg = intval(date('G')); //Hour
+echo $nowg;
--- php/dir.php
+++ php/dir.php
@@ -3,13 +3,9 @@ $directory = opendir('../');
 echo "Files are:<br>\n";
 $x = 0;
 while ($file = readdir($directory)) {
-    if (chown($file, "john")) {
-         $x = $x + 1;
-    }
-    else{}
     echo "$file <br>\n";
 }
-echo $x;
+// echo $x;
 
-$sfsf = [];
-array_fill_keys([1,2,3], $sfsf);
+// $sfsf = [];
+// array_fill_keys([1,2,3], $sfsf);
